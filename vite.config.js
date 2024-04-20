94abd54570795ad9c2f9f1ccb61778fef9fa7d02
n-satyaprakash <snandigam@my.okcu.edu>
2024-04-19 20:48:14 -0500

    3rd commit

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  build: {
-    outDir: 'build', // Make sure this is correctly set
-  },
-  plugins: [react()]
+  base: '/hw5/', 
+  plugins: [react()],
 });
